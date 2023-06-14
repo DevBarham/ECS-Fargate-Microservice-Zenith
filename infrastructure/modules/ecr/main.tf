00d60f8997f13888905b08c1aed7ea60e33ca70b
eniolastyle <amiolastyle@gmail.com>
2023-06-14 11:29:58 +0100

    dynamo ecr ...

---
+++ infrastructure/modules/ecr/main.tf
@@ -0,0 +1,9 @@
+/*=========================================
+     Elastic Container Repository
+==========================================*/
+
+resource "aws_ecr_repository" "ecr_repository" {
+  name                 = var.name
+  image_tag_mutability = "MUTABLE"
+  force_delete         = true
+}
